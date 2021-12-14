80147e6553db63ef7ca7bb250b70247f112d9c60
Terry Xu <knight3001@gmail.com>
2021-12-14 15:44:59 +1000

    fix App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+test("render without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
 });
